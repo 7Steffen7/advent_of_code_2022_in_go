52cf0c4027bec4590a5a25fb61efd81637230324
Steffen Parth <sparth@1-E-20.42heilbronn.de>
2024-11-11 15:42:23 +0100

    Day 3 2/2

---
+++ Day_3.1/rucksack_reorganization.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func convertValue(value byte) int {
+	if value >= 'a' && value <= 'z' {
+		return int(value - 96)
+	} else {
+		return int(value - 38)
+	}
+}
+
+// func sharedLetter(first_str string, second_str string) byte {
+// 	for i := 0; i < len(first_str); i++ {
+// 		var temp_string string = string(first_str[i])
+// 		if strings.Contains(second_str, temp_string) {
+// 			return first_str[i]
+// 		}
+// 	}
+// 	return 0
+// }
+
+func getSharedValue(arr []string) byte {
+	for i := 0; i < len(arr[0]); i++ {
+		temp_string := string(arr[0][i])
+		if strings.Contains(arr[1], temp_string) && strings.Contains(arr[2], temp_string) {
+			return arr[0][i]
+		}
+	}
+	return 0
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var sum int
+	s := make([]string, 3)
+	for i := 0; scanner.Scan(); i++ {
+		line := scanner.Text()
+		switch i {
+		case 0:
+			s[0] = line
+		case 1:
+			s[1] = line
+		case 2:
+			s[2] = line
+			value := getSharedValue(s)
+			sum += convertValue(value)
+			i = -1
+		}
+
+		// string_size := len(line)
+
+		// first_string := line[:string_size/2]
+		// second_string := line[string_size/2:]
+		// letter := sharedLetter(first_string, second_string)
+		// temp := convertValue(letter)
+		// sum += temp
+		// fmt.Printf("letter: %+q", letter)
+		// fmt.Println("value: ", temp)
+		// fmt.Println("shared letter:", letter)
+		// fmt.Print("string: ", line)
+		// fmt.Printf(" first: %s, second %s", first_string, second_string)
+		// fmt.Println("string size:", string_size)
+	}
+	fmt.Println("sum:", sum)
+
+}
