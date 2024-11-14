e5f7bc0de2f35b150ab5ea6b4a09ac6c9985251c
Steffen Parth <steffen.parth@icloud.com>
2024-11-14 14:08:46 +0100

    day 6 1/2

---
+++ Day_6.0/tuning_trouble.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func peekForward(subSlice []byte) bool {
+	// fmt.Printf("%s \n", subSlice)
+	for i := 0; i < len(subSlice); i++ {
+		for j := 1; j+i < len(subSlice); j++ {
+			if subSlice[i] == subSlice[i+j] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func main() {
+
+	data, err := os.ReadFile("input.txt")
+	check(err)
+
+	// var input string = string(data)
+	// fmt.Println(input)
+	for i := 0; i+4 < len(data); i++ {
+		if peekForward(data[i : i+4]) {
+			fmt.Println("nbr:", i+4)
+			break
+		}
+	}
+}
